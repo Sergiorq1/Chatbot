c486f74fd8aa0d507d20cc8d871e7dd4f12b994d
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-09-21 12:11:41 -0700

    Three topic setup
    music, space, food

--- Chatbot.py
+++ Chatbot.py
@@ -1,1 +1,33 @@
-print("HI")
+
+from random import choice
+def get_box_response(user_response):
+  bot_response_music = ["omg! great!", "Keep smiling!", "I love to see you happy!"]
+  bot_response_space = ["im here for you", "sending good vibes"]
+  bot_response_food = ["My favorite food is sushi, what's yours?"]
+
+  if user_response == "Music" or "music":
+    return choice(bot_response_music)
+  elif user_response == "Space" or "space":
+    return choice(bot_response_space)
+  elif user_response == "Food" or "food":
+    return choice(bot_response_food)
+  else:
+    return "Wow, I do not know what that is. Tell me more!"
+
+
+
+
+print("Welcome to Chat Bot")
+print("What do you want to talk about? (Ex: Music,Space,Food)")
+
+user_response = ""
+#TODO: we want to keep repeating until the user enters "done" what should we put here?
+while True:
+  user_response = input("How are you feeling today?: ")
+  
+  # Quits program when user responds with 'done'
+  if user_response == 'done':
+    break
+
+  
+  print(get_bot_response(user_response))
