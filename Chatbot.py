d358916b718cd5d636985d6475e65401253c4ea6
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-09-22 22:27:31 -0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,33 +1,26 @@
 
 from random import choice
-def get_box_response(user_response):
-  bot_response_music = ["omg! great!", "Keep smiling!", "I love to see you happy!"]
-  bot_response_space = ["im here for you", "sending good vibes"]
-  bot_response_food = ["My favorite food is sushi, what's yours?"]
+print("Welcome to Chat Bot")
 
-  if user_response == "Music" or "music":
+def get_box_response(user_response):
+  bot_response_music = ["Ya like jazz?", "Keep smiling!", "I love to see you happy!"]
+  bot_response_space = ["im here for you", "sending good vibes"]
+  bot_response_food = ["My favorite food is sushi, what's yours?", "Pinapple belongs on pizza, sorry to break the news", "Lettuce is just crunchy water"]
+
+  if "Music" in user_response or "music" in user_response:
     return choice(bot_response_music)
-  elif user_response == "Space" or "space":
+  elif "Space" in user_response or "space" in user_response:
     return choice(bot_response_space)
-  elif user_response == "Food" or "food":
+  elif "Food" in user_response or "food" in user_response:
     return choice(bot_response_food)
   else:
     return "Wow, I do not know what that is. Tell me more!"
 
 
-
-
-print("Welcome to Chat Bot")
-print("What do you want to talk about? (Ex: Music,Space,Food)")
-
-user_response = ""
-#TODO: we want to keep repeating until the user enters "done" what should we put here?
 while True:
-  user_response = input("How are you feeling today?: ")
-  
-  # Quits program when user responds with 'done'
+  user_response = input("What do you want to talk about? (Ex: Music,Space,Food)")
   if user_response == 'done':
     break
 
   
-  print(get_bot_response(user_response))
+  print(get_box_response(user_response))
